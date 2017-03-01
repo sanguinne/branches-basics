f516bf257228e68f45d67732cadafc0fe7a0ea88
Данилина Анна Сергеевна <ri67s03@mwp11.csc.local>
2017-03-01 08:35:37 +0700

    Change the function main

--- introduce.c
+++ introduce.c
@@ -2,9 +2,13 @@
 
 int main()
 {
+    int age;
     char name[15];
     printf("hello! What's your name?\n");
     gets(name);
     printf("hello, %s!\n", name);
+    printf("How old are you?\n");
+    scanf("%d", &age);
+    printf("%s, you're %d years old.", name, age);
     return 0;
 }
