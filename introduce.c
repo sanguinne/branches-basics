e925cc7199b92b5c81345c09d4597d140f6ee5a4
Данилина Анна Сергеевна <ri67s03@mwp11.csc.local>
2017-03-01 08:52:02 +0700

    Add new commit in this branch

--- introduce.c
+++ introduce.c
@@ -9,6 +9,7 @@ int main()
     printf("hello, %s!\n", name);
     printf("How old are you?\n");
     scanf("%d", &age);
-    printf("%s, you're %d years old.", name, age);
+    printf("%s, you're %d years old.\n", name, age);
+    printf("Nice to meet you, %s", name);
     return 0;
 }
